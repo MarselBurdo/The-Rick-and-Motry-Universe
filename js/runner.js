15f8d7b545d198429678eb5b96f2436a803a9257
MarselBurdo <9003363@gmail.com>
2020-07-11 16:42:56 +0300

    almost done

--- js/runner.js
+++ js/runner.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 /* eslint-disable linebreak-style */
 /* eslint-disable require-jsdoc */
 // const rickMarty = require('rickmortyapi')
@@ -6,50 +7,134 @@ const selectChoise = document.getElementById('searchType');
 const word = document.getElementsByClassName('is-large input')[0];
 const spinner = document.getElementsByClassName('spinner')[0];
 const content = document.getElementById('content');
+const article = document.getElementsByTagName('article')
 const resultContainer = document.getElementById('result-container');
 const actionBtn = document.getElementById('byQueryBtn');
 
 actionBtn.addEventListener('click', () => {
   function searchByName(value) {
-    resultContainer.style.visibility ='hidden'
+    resultContainer.style.visibility = 'hidden';
     spinner.style.visibility = 'visible';
+
     fetch(`https://rickandmortyapi.com/api/character/?name=${value}`)
-        .then((res) => res.json())
-        .then((data) => renderResult(data.results))
+      .then((res) => res.json())
+      .then((data) => {
+        let result = data.results;
+        const pages = data.info.pages;
+        if (pages > 1) {
+          for (i = 2; i <= pages; i++) {
+            const page = i;
+            fetch(`https://rickandmortyapi.com/api/character/?page=${i}&name=${value}`)
+              .then((res) => res.json())
+              .then((data) => {
+                result = result.concat(data.results);
+                if (page === pages) {
+                  renderResultPeople(result);
+                }
+              });
+          }
+        } else {
+          renderResultPeople(result);
+        }
+      });
     setTimeout(() => {
       spinner.style.visibility = 'hidden';
-      resultContainer.style.visibility ='visible'
+      resultContainer.style.visibility = 'visible';
     }, 3000);
   }
+
   function searchByEpisode(value) {
-    resultContainer.style.visibility ='hidden'
+    resultContainer.style.visibility = 'hidden';
     spinner.style.visibility = 'visible';
     fetch(`https://rickandmortyapi.com/api/episode/${value}`)
-        .then((res) => res.json())
-        .then((data) => console.log(data));
+      .then((res) => res.json())
+      .then((data) => {
+        let tmpArr = []
+        data.characters.forEach(el => {
+          fetch(`${el}`)
+            .then((res) => res.json())
+            .then(data => {
+              tmpArr.push(data.name)
+            })
+        })
+        // console.log(tmpArr)
+        renderResult(data, tmpArr);
+
+
+      });
     setTimeout(() => {
       spinner.style.visibility = 'hidden';
-      resultContainer.style.visibility ='visible'
+      resultContainer.style.visibility = 'visible';
     }, 3000);
   }
   function searchByLocation(value) {
-    resultContainer.style.visibility ='hidden'
+    resultContainer.style.visibility = 'hidden';
     spinner.style.visibility = 'visible';
-    fetch(`https://rickandmortyapi.com/api/location/${value}`)
-        .then((res) => res.json())
-        .then((data) => console.log(data));
+    fetch(`https://rickandmortyapi.com/api/location/?name=${value}`)
+      .then((res) => res.json())
+      .then((data) => {
+        let result = data.results;
+        const pages = data.info.pages;
+        if (pages > 1) {
+          for (i = 2; i <= pages; i++) {
+            const page = i;
+            fetch(`https://rickandmortyapi.com/api/location/?page=${i}&name=${value}`)
+              .then((res) => res.json())
+              .then((data) => {
+                result = result.concat(data.results);
+                if (page === pages) {
+                  renderResultPeople(result);
+                }
+              });
+          }
+        } else {
+          renderResultPeople(result);
+        }
+      });
+
     setTimeout(() => {
       spinner.style.visibility = 'hidden';
-      resultContainer.style.visibility ='visible'
+      resultContainer.style.visibility = 'visible';
     }, 3000);
   }
-  function renderResult(objValue) {
-    content.innerHTML='';
-    content.innerHTML= `
-    <img src ="${obj.image}"'>`
 
 
+  function renderResultPeople(objValue) {
+    content.innerHTML = '';
+    content.innerText = '';
+    let tmp;
+    console.log(objValue);
+    objValue.forEach((value) => {
+      if (value.image){
+        tmp += `
+        <img  class='outputImage' src ="${value.image}"'>
+        <div class ='outputInfo'>${value.name}</div>
+        <div class ='outputInfo'>${value.dimension}</div>
+        <br><br><br><br><br>
+        `;
+      } else {
+        tmp += `
+        <div class ='outputInfo'>${value.name}</div>
+        <div class ='outputInfo'>${value.dimension}</div><br><br><br><br><br>
+        `;
+
+      }
+    });
+
+    content.innerHTML = tmp;
   }
+  function renderResult(objValue, arr) {
+
+    console.log(arr);
+    content.innerHTML = `<div class ='outputInfo'>${objValue.name}</div>`;
+    // content.innerHTML = '';
+    // let tmp;
+    // arr.forEach(el => {
+    //   tmp += `<div class ='outputInfo'>${el}</div>`;
+    // })
+    // content.innerHTML = tmp;
+  }
+
   switch (selectChoise.selectedIndex) {
     case 0:
       searchByName(word.value);
