83ed6763b1149bae2a1c167563b6fbc9464efae2
MarselBurdo <9003363@gmail.com>
2020-07-11 08:14:14 +0300

    spiner-work, api-work

--- js/runner.js
+++ js/runner.js
@@ -1,1 +1,51 @@
-const rickMotry = require('rickmortyapi')
+/* eslint-disable linebreak-style */
+/* eslint-disable require-jsdoc */
+// const rickMarty = require('rickmortyapi')
+
+const selectChoise = document.getElementById('searchType');
+const word = document.getElementsByClassName('is-large input')[0];
+const spinner = document.getElementsByClassName('spinner')[0];
+const content = document.getElementById('content');
+const resultContainer = document.getElementById('result-container');
+const actionBtn = document.getElementById('byQueryBtn');
+
+actionBtn.addEventListener('click', () => {
+  function searchByName(value) {
+    fetch(`https://rickandmortyapi.com/api/character/?name=${value}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    spinner.style.visibility = 'visible';
+    setTimeout(() => {
+      spinner.style.visibility = 'hidden';
+    }, 3000);
+  }
+  function searchByEpisode(value) {
+    fetch(`https://rickandmortyapi.com/api/episode/${value}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    spinner.style.visibility = 'visible';
+    setTimeout(() => {
+      spinner.style.visibility = 'hidden';
+    }, 3000);
+  }
+  function searchByLocation(value) {
+    fetch(`https://rickandmortyapi.com/api/location/${value}`)
+        .then((res) => res.json())
+        .then((data) => console.log(data));
+    spinner.style.visibility = 'visible';
+    setTimeout(() => {
+      spinner.style.visibility = 'hidden';
+    }, 3000);
+  }
+  switch (selectChoise.selectedIndex) {
+    case 0:
+      searchByName(word.value);
+      break;
+    case 1:
+      searchByEpisode(word.value);
+      break;
+    case 2:
+      searchByLocation(word.value);
+      break;
+  }
+});
